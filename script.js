64623e1a644e8246a1ca8b731c5e2d1e366f1ed6
wilkinsst2-pixel <wilkinsst2@vcu.edu>
2026-05-06 02:06:04 -0400

    improvements

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+const navLinks = document.querySelectorAll('nav a');
+const sections = document.querySelectorAll('.container');
 const themeBtn = document.getElementById('theme-btn');
 
 themeBtn.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
+});
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+
+    if (scrollY >= sectionTop - 200) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === `#${current}`) {
+      link.classList.add('active');
+    }
+  });
 });
